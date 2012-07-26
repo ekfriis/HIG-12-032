c01fcd9635a13d0205d7ad87de7ba14371f37ffc
Evan Friis <evan.friis@cern.ch>
2012-07-26 10:12:41 -0500

    Script to combine cards

---
+++ make_megacard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COMBO_DIR=$1
+
+combineCards.py \
+  boost=$COMBO_DIR/cmb/125/htt_tt_0_8TeV.txt \
+  vbf=$COMBO_DIR/cmb/125/htt_tt_1_8TeV.txt \
+  llt7TeV=$COMBO_DIR/cmb/125/vhtt_0_7TeV.txt \
+  llt8TeV=$COMBO_DIR/cmb/125/vhtt_0_8TeV.txt \
+  ZH8TeV=$COMBO_DIR/cmb/125/vhtt_1_8TeV.txt \
+  ZH7TeV=$COMBO_DIR/cmb/125/vhtt_1_7TeV.txt > megacard_125.txt
